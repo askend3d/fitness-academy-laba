c86f7f587a7d7f32cc51aa6fd990f975cc7793f5
p9co <ramazan01012005@gmail.com>
2023-12-07 23:21:28 +0300

    commit\

---
+++ FitnessAcademy/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FitnessAcademy
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Academy_fitnessEntities context = new Academy_fitnessEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegistrationGrid.ItemsSource = context.CourseRegistration.ToList();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var newRegsitration = new CourseRegistration();
+            context.CourseRegistration.Add(newRegsitration);
+            var win = new RegistrationCourseWindow(context, newRegsitration);
+            win.ShowDialog();
+            RegistrationGrid.ItemsSource = context.CourseRegistration.ToList();
+        }
+
+        private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var row = RegistrationGrid.SelectedItems as CourseRegistration;
+            if (row == null)
+            {
+                MessageBox.Show("Строка не выбрана");
+                return;
+            }
+
+            context.CourseRegistration.Remove(row);
+            context.SaveChanges();
+            RegistrationGrid.ItemsSource = context.CourseRegistration.ToList();
+
+        }
+
+        private void EditButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button EditButton = sender as Button;
+            var currentRegistration = EditButton.DataContext as CourseRegistration;
+            var win = new RegistrationCourseWindow(context, currentRegistration);
+            win.ShowDialog();
+        }
+    }
+}
---
+++ FitnessAcademy/RegistrationCourseWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FitnessAcademy
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationCourseWindow.xaml
+    /// </summary>
+    public partial class RegistrationCourseWindow : Window
+    {
+        Academy_fitnessEntities context;
+        public RegistrationCourseWindow(Academy_fitnessEntities context, CourseRegistration currentRegistration)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbCourse.ItemsSource = context.Course.ToList();
+            CmbTrainer.ItemsSource = context.Trainer.ToList();
+
+            this.DataContext = currentRegistration;
+
+
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (CheckRegistration())
+            {
+                context.SaveChanges();
+                this.Close();
+            }
+        }
+
+        private bool CheckRegistration()
+        {
+            var reg = this.DataContext as CourseRegistration;
+            if (reg.Trainer == null)
+            {
+                MessageBox.Show("Тренер не выбран");
+                return false;
+            }
+
+            if (reg.Course == null)
+            {
+                MessageBox.Show("Курсы не выбраны");
+                return false;
+            }
+
+            if (reg.CreatedDate == null)
+            {
+                MessageBox.Show("Дата не выбрана");
+                return false;
+            }
+
+            if (!int.TryParse(TxtTotalPoint.Text, out int result))
+            {
+                MessageBox.Show("Выбраны неверные данные");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
